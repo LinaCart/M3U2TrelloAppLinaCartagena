6fb8953dfe1000e43b679a4cabaa9a286d62cdda
LinaCart <catahecarica22@hotmail.com>
2022-08-18 21:00:05 -0500

    BD actualizada

--- index.js
+++ index.js
@@ -68,8 +68,6 @@ axios
     })
 
     const sor = new Sortable.default(
-        //Referenciamos todos los container-tasks
         document.querySelectorAll('.container-tasks'),
-        //Hacemos ordenables todos los article de las tareas
         { draggable: 'article' }
       );
